243ead9b2f2399b38695a7a09386a092351d4430
Naman Gupta <i-naman.gupta@monsterindia.com>
2023-01-16 23:00:18 +0530

    small change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,22 +6,16 @@ import styles from "./Header.module.css";
 
 function Header() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Make your own resume. <span>It's free</span>
+        </p>
+      </div>
     </div>
-    // <div className={styles.container}>
-    //   <div className={styles.left}>
-    //     <p className={styles.heading}>
-    //       A <span>Resume</span> that stands out!
-    //     </p>
-    //     <p className={styles.heading}>
-    //       Make your own resume. <span>It's free</span>
-    //     </p>
-    //   </div>
-    //   <div className={styles.right}>
-    //     <img src={resumeSvg} alt="Resume" />
-    //   </div>
-    // </div>
   );
 }
 
